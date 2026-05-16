0ae106a67f12d29dafdf223a0da09d779a43755d
AlwaleedAlroomi <alwaleed101az@gmail.com>
2026-05-16 23:42:39 +0300

    add InvoiceNumberSequence model + migration + factory

---
+++ database/factories/InvoiceNumberSequenceFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\InvoiceNumberSequence;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<InvoiceNumberSequence>
+ */
+class InvoiceNumberSequenceFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
