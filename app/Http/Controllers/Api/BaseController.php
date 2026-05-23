e7eb60ad4803dfe4bff8739d409d2da44a80372b
AlwaleedAlroomi <alwaleed101az@gmail.com>
2026-05-23 22:39:37 +0300

    add baseController class

---
+++ app/Http/Controllers/Api/BaseController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
+
+class BaseController extends Controller
+{
+    /**
+     * Success response method.
+     *
+     * @param  mixed  $result data to return
+     * @param  string  $message
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function sendResponse($result, string $message): JsonResponse
+    {
+        $response = [
+            'success' => true,
+            'message' => $message,
+            'data' => $result,
+        ];
+
+        return response()->json($response, 200);
+    }
+
+    /**
+     * Return error response.
+     *
+     * @param  string  $error
+     * @param  array  $errorMessages
+     * @param  int  $code
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function sendError(string $error, array $errorMessages = [], int $code = 404): JsonResponse
+    {
+        $response = [
+            'success' => false,
+            'message' => $error,
+        ];
+
+        if (!empty($errorMessages)) {
+            $response['data'] = $errorMessages;
+        }
+
+        return response()->json($response, $code);
+    }
+}
