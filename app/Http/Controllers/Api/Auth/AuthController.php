fb0f13d4e25a3e24873f39afe04dfcef81b4e04f
AlwaleedAlroomi <alwaleed101az@gmail.com>
2026-05-25 23:29:32 +0300

    login & logout & me function added

--- app/Http/Controllers/Api/Auth/AuthController.php
+++ app/Http/Controllers/Api/Auth/AuthController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Api\BaseController;
 use App\Http\Requests\Auth\RegisterUserRequest;
 use App\Http\Requests\Auth\LoginUserRequest;
 use App\Http\Resources\User\AuthResource;
+use App\Http\Resources\User\UserResource;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
@@ -38,9 +39,33 @@ class AuthController extends BaseController
         return $this->sendResponse($transformedData, 'Account Created successfully', 201);
     }
 
-    public function login(LoginUserRequest $request) {}
+    public function login(LoginUserRequest $request)
+    {
+        $validated = $request->validated();
 
-    public function logout(Request $request) {}
+        $user = User::where('email', $validated['email'])->first();
 
-    public function user(Request $request) {}
+        if (!$user || !Hash::check($validated['password'], $user->password)) {
+            return $this->sendError('Invalid credentials', code: 401);
+        }
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        $authData = ['user' => $user, 'token' => $token, 'token_type' => 'Bearer'];
+
+        $transformedData = new AuthResource((object) $authData);
+
+        return $this->sendResponse($transformedData, 'Successfully login', 201);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+        return $this->sendResponse(result: null, message: 'Logged out successfully');
+    }
+
+    public function me(Request $request)
+    {
+        return $this->sendResponse(new UserResource($request->user()), 'User data retrieved successfully');
+    }
 }
